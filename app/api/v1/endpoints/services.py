9f9527bf2a7efe39c3b83520177fa7348579f0d3
MelonDev <melondev@icloud.com>
2022-06-05 19:35:51 +0700

    ทดสอบ endpoint

--- app/api/v1/endpoints/services.py
+++ app/api/v1/endpoints/services.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 
-
 router = APIRouter()
 
 
 @router.get("/")
 async def testing_child_resource():
     return {"message": "Hi There! This is my route endpoint."}
+
+
+@router.get("/hello/{x}")
+async def hello(x: int):
+    return {"message": f"Hello! :{x * 543}"}
