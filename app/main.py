e1c7f622b62ae8766311f252795ff2ad52d633cc
MelonDev <melondev@icloud.com>
2022-06-05 19:00:21 +0700

    แก้ไขไฟล์ main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import uvicorn
 
 from api.v1.api import router as api_router
 
 app = FastAPI(title='Serverless Lambda FastAPI')
 
 app.include_router(api_router, prefix="/api/v1")
+handler = Mangum(app)
 
 
 @app.get("/", tags=["Endpoint Test"])
@@ -13,4 +15,5 @@ def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
 
-handler = Mangum(app, lifespan="off")
+if __name__ == '__main__':
+    uvicorn.run(app)
