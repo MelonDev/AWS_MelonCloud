15553fc01ad930ef58b9d6c6ec12dba0fd29d93d
MelonDev <melondev@icloud.com>
2022-06-09 02:17:45 +0700

    อัปเดต meloncloud

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from mangum import Mangum
 from api.v1.api import router as api_router
 import uvicorn
 
+from app.environment.environment import SECRET_KEY
+
 app = FastAPI(title='MelonCloud', openapi_prefix="/stage")
 
 app.include_router(api_router, prefix="/api/v1")
@@ -12,6 +14,11 @@ app.include_router(api_router, prefix="/api/v1")
 @app.get("/", tags=["Main"])
 def main():
     return {"message": "Hello! I'm MelonCloud"}
+
+
+@app.get("/env", tags=["Main"])
+def env():
+    return {"message": SECRET_KEY}
 
 
 handler = Mangum(app=app)
